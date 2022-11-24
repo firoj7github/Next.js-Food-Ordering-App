ce299aed2b92bc004572585121b7a9708737e38f
firoj <fkfiroj02@gmail.com>
2022-11-24 08:03:25 -0800

    handel

--- pages/api/product/[pid].js
+++ pages/api/product/[pid].js
@@ -1,6 +1,6 @@
 import Product from '../../../models/Product'
 
-export default async(req,res)=>{
+export default async function handler(req,res){
 const {pid}=req.query;
  const product= await Product.findOne({_id:pid});
  res.status(200).json(product);
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -7,7 +7,7 @@ import Product from "../../../models/Product"
 
   dbConnect();
 
-  export default (req,res)=> {
+  export default function handler (req,res) {
     Product.find().then(products=>{
       res.status(200).json(products);
     })
