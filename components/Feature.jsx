213b09cdaed54f75e3d2e66ef0c82eefbb859605
firoj <fkfiroj02@gmail.com>
2022-11-26 08:11:54 -0800

    nice

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export const Feature = () => {
   return (
     <div>
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <Image src="/image/2000.jpg" class="d-block " width='1400' height='400' alt="..."/>
+        <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <Image src="/image/2000.jpg" className="d-block " width='1400' height='400' alt="..."/>
     </div>
     
   </div>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -130,15 +130,15 @@ export const Footer = () => {
 
 
   
-  <section class=" bg-black">
-    <div class="container text-center text-md-start ">
+  <section className=" bg-black">
+    <div className="container text-center text-md-start ">
      
-      <div class="row ">
+      <div className="row ">
        
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
        
-          <h6 class="text-uppercase fw-bold mb-4 mt-3">
-            <i class="fas fa-gem  text-secondary "></i>Company name
+          <h6 className="text-uppercase fw-bold mb-4 mt-3">
+            <i className="fas fa-gem  text-secondary "></i>Company name
           </h6>
           <p>
             Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -146,54 +146,54 @@ export const Footer = () => {
           </p>
         </div>
        
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
     
-          <h6 class="text-uppercase fw-bold mb-4 mt-3">
+          <h6 className="text-uppercase fw-bold mb-4 mt-3">
             Products
           </h6>
           <p>
-            <a href="#!" class="text-reset">Potato pizza</a>
+            <a href="#!" className="text-reset">Potato pizza</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">pizza fai</a>
+            <a href="#!" className="text-reset">pizza fai</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">cikhen pizza</a>
+            <a href="#!" className="text-reset">cikhen pizza</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">pizza muthun</a>
+            <a href="#!" className="text-reset">pizza muthun</a>
           </p>
         </div>
       
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
     
-          <h6 class="text-uppercase fw-bold mb-4 mt-3">
+          <h6 className="text-uppercase fw-bold mb-4 mt-3">
             Useful links
           </h6>
           <p>
-            <a href="#!" class="text-reset">Pricing</a>
+            <a href="#!" className="text-reset">Pricing</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Settings</a>
+            <a href="#!" className="text-reset">Settings</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Orders</a>
+            <a href="#!" className="text-reset">Orders</a>
           </p>
           <p>
-            <a href="#!" class="text-reset">Help</a>
+            <a href="#!" className="text-reset">Help</a>
           </p>
         </div>
       
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
       
-          <h6 class="text-uppercase fw-bold mb-4 mt-3">Contact</h6>
-          <p><i class="fas fa-home  text-secondary"></i> Kushtia, NS Road 10012</p>
+          <h6 className="text-uppercase fw-bold mb-4 mt-3">Contact</h6>
+          <p><i className="fas fa-home  text-secondary"></i> Kushtia, NS Road 10012</p>
           <p>
-            <i class="fas fa-envelope  text-secondary"></i>
+            <i className="fas fa-envelope  text-secondary"></i>
             info@example.com
           </p>
-          <p><i class="fas fa-phone  text-secondary"></i> + 04 234 567 88</p>
-          <p><i class="fas fa-print  text-secondary"></i> + 05 234 567 89</p>
+          <p><i className="fas fa-phone  text-secondary"></i> + 04 234 567 88</p>
+          <p><i className="fas fa-print  text-secondary"></i> + 05 234 567 89</p>
         </div>
         
       </div>
